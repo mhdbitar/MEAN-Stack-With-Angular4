65680661baf58f7547d4ec5656ffd53d6f20a2fb
mhd <mreash@live.com>
2017-08-22 11:24:32 +0300

    second commeit

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ mongoose.connect(config.uri, (err) => {
   if (err) {
     console.log('Could not connect to database: ', err);
   } else {
-    console.log(config.secret);
     console.log('Connected to database: ', config.db);
   }
 });
